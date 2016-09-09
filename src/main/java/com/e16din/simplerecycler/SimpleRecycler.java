41d18aa6439cef9716aeb78782498d3fb7de164f
e16din <bigusov@gmail.com>
2016-09-09 19:50:41 +0300

    Refactor SimpleRecycler.

--- src/main/java/com/e16din/simplerecycler/SimpleRecycler.java
+++ src/main/java/com/e16din/simplerecycler/SimpleRecycler.java
@@ -1,10 +1,13 @@
-package com.e16din.simplerecycler.adapter;
+package com.e16din.simplerecycler;
 
 import android.content.Context;
 
 import com.e16din.handyholder.wrapper.Handy;
 
-public class SimpleRecycler {
+public final class SimpleRecycler {
+
+    private SimpleRecycler() {
+    }
 
     public static void init(Context context) {
         Handy.init(context);
