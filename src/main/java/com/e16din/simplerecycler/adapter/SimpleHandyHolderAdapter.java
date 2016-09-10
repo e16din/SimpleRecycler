a61aa1f9180e4ff30cca20aa1a5d3c62223690aa
e16din <bigusov@gmail.com>
2016-09-10 10:19:47 +0300

    Remove unused methods.

--- src/main/java/com/e16din/simplerecycler/adapter/SimpleHandyHolderAdapter.java
+++ src/main/java/com/e16din/simplerecycler/adapter/SimpleHandyHolderAdapter.java
@@ -7,8 +7,6 @@ import android.view.ViewGroup;
 
 import com.e16din.handyholder.AlreadyBox;
 import com.e16din.handyholder.holder.HandyHolder;
-import com.e16din.handyholder.listeners.click.OnClickListener;
-import com.e16din.handyholder.listeners.click.OnViewsClickListener;
 import com.e16din.handyholder.wrapper.Handy;
 
 import java.util.List;
@@ -72,36 +70,5 @@ public abstract class SimpleHandyHolderAdapter<HOLDER extends RecyclerView.ViewH
             if (mClickableViewsList != null)
                 set.setViewsClickListener(mClickableViewsList, mOnItemViewsClickListener);
         }
-    }
-
-
-    public void setOnItemClickListener(OnClickListener<MODEL> listener) {
-        mOnItemClickListener = listener;
-    }
-
-
-    public void setOnItemViewsClickListener(List<Integer> clickableViews, OnViewsClickListener<MODEL> listener) {
-        mClickableViewsList = clickableViews;
-        mOnItemViewsClickListener = listener;
-    }
-
-    public void setOnItemViewsClickListener(int[] clickableViews, OnViewsClickListener<MODEL> listener) {
-        mClickableViewsArray = clickableViews;
-        mOnItemViewsClickListener = listener;
-    }
-
-
-    public OnClickListener<MODEL> getOnItemClickListener() {
-        return mOnItemClickListener;
-    }
-
-
-    public OnViewsClickListener<MODEL> getOnViewsClickListener() {
-        return mOnItemViewsClickListener;
-    }
-
-
-    public List<Integer> getClickableViewsList() {
-        return mClickableViewsList;
     }
 }
